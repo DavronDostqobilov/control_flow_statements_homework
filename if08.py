72e632fb9df39feb5ceb376d03669df1a4ada32e
DavronDostqobilov <davrondostqobilov885@gmail.com>
2022-11-22 12:32:26 +0300

    if08

--- if08.py
+++ if08.py
@@ -11,4 +11,12 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a//10!=0 and a%2!=0:
+        return 'two-digit odd number'
+    if a//10!=0 and a%2==0:
+        return 'two-digit even number'
+    if a//100!=0 and a%2!=0:
+        return 'three-digit odd number'
+    if a//100!=0 and a%2==0:
+        return 'three-digit even number'
+print(main(24))
